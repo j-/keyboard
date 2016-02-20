3e6d4d0c2434871b990623ae9c7d461a5a025e16
j- <j@skeoh.com>
2016-02-21 01:06:42 +1100

    Log key down event details

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import 'normalize.css';
 import './styles/main.less';
 
+import keynames from './keynames';
+
 import CapslockModifierState from './classes/modifier-states/CapslockModifierState';
 import NumlockModifierState from './classes/modifier-states/NumlockModifierState';
 
@@ -14,3 +16,10 @@ caps.on('change', (state) => console.log('caps', state));
 const num = new NumlockModifierState();
 num.start();
 num.on('change', (state) => console.log('num', state));
+
+window.addEventListener('keydown', (e) => {
+	const { which, location } = e;
+	const name = keynames[which];
+	const hex = (which < 0x10 ? '0' : '') + which.toString(16);
+	console.log('keydown', { hex, which, location, name });
+});
