7807b6c1129f05ef1d478c84e5bb78c14c85ede8
j- <j@skeoh.com>
2016-02-21 17:23:32 +1100

    Add simple modifiers table

--- src/classes/components/Modifiers.jsx
+++ src/classes/components/Modifiers.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
 export default class Modifiers extends React.Component {
+	getModifiers () {
+		return [
+			{ name: 'CapsLock', state: null },
+			{ name: 'NumLock', state: null },
+			{ name: 'ScrollLock', state: null },
+		];
+	}
+
+	getRows () {
+		return this.getModifiers().map((modifier) => {
+			return <div key={modifier.name}>
+				<strong>{modifier.name}</strong>:
+				<em>{String(modifier.state)}</em>
+			</div>;
+		});
+	}
+
 	render () {
-		return <div>React component</div>;
+		return <div>{ this.getRows() }</div>;
 	}
 }
