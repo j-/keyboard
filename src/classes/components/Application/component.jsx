f6f7bf1aa7b8bc9b207e613fd28594c5486a4b15
j- <j@skeoh.com>
2016-03-06 13:05:53 +1100

    Add page header

--- src/classes/components/Application/component.jsx
+++ src/classes/components/Application/component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { PageHeader } from 'react-bootstrap';
 import ModifierTable from '../ModifierTable/component';
 import FocusLockButton from '../FocusLockButton/component';
 
@@ -6,6 +7,7 @@ export default class Application extends React.Component {
 	render () {
 		return (
 			<div className="container">
+				<PageHeader>Keyboard</PageHeader>
 				<FocusLockButton />
 				<ModifierTable />
 			</div>
