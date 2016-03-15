26eaa3e58174a06425cf21be5dc55804124df0cf
j- <j@skeoh.com>
2016-03-15 21:16:44 +1100

    Add missing comma to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
 	entry: './src/main.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js'
+		filename: 'bundle.js',
 	},
 	resolve: {
 		extensions: ['', '.js', '.jsx'],
