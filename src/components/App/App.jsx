93ea0ce82a8542db58bc48984cd601b9d11f56a8
j- <j@skeoh.com>
2017-02-12 19:34:05 +1100

    Add test area to active modifiers section

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,6 +9,7 @@ const App = () => (
 		<h1>Keyboard</h1>
 
 		<h2>Active Modifiers</h2>
+		<TestArea />
 		<p>Determined using <a href="https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/getModifierState" title="'KeyboardEvent.getModifierState()' on MDN"><code>KeyboardEvent.getModifierState()</code></a>. Press any key to determine the state of all modifiers. When the page loses focus the state of each modifier key is invalidated.</p>
 		<ModifierList />
 
