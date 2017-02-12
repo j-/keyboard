be8a7f9f716da274287ea040bead041e80e1a06f
j- <j@skeoh.com>
2017-02-12 21:08:00 +1100

    Add React Router

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,25 +4,56 @@ import EventLightList from '../EventLightList';
 import EventDetailsList from '../EventDetailsList';
 import TestArea from '../TestArea';
 
-const App = () => (
-	<div>
-		<h1>Keyboard</h1>
+import {
+	MemoryRouter as Router,
+	Route,
+	Link,
+} from 'react-router-dom'
 
+const PageActiveModifiers = () => (
+	<div>
 		<h2>Active Modifiers</h2>
 		<TestArea />
 		<p>Determined using <a href="https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/getModifierState" title="'KeyboardEvent.getModifierState()' on MDN"><code>KeyboardEvent.getModifierState()</code></a>. Press any key to determine the state of all modifiers. When the page loses focus the state of each modifier key is invalidated.</p>
 		<ModifierList />
+	</div>
+);
 
+const PageTriggeredEvents = () => (
+	<div>
 		<h2>Triggered Events</h2>
 		<TestArea />
 		<p>Event listeners on the window object show when these events are fired. Each light will flash green for 100ms after it has been pressed. It will then turn grey and fade out. You may notice that not all keys trigger a keypress event.</p>
 		<EventLightList />
+	</div>
+);
 
+const PageLatestEvents = () => (
+	<div>
 		<h2>Latest Events</h2>
 		<TestArea />
 		<p>Observe the order and details of each keyboard event captured by the window. Shows the nine latest keyboard events with the most recent at the top.</p>
 		<EventDetailsList />
 	</div>
+);
+
+const App = () => (
+	<Router
+		initialEntries={ ['/active-modifiers'] }
+		initialIndex={ 1 }
+	>
+		<div>
+			<h1>Keyboard</h1>
+			<ul>
+				<li><Link to="/active-modifiers">Active Modifiers</Link></li>
+				<li><Link to="/trigered-events">Triggered Events</Link></li>
+				<li><Link to="/latest-events">Latest Events</Link></li>
+			</ul>
+			<Route path="/active-modifiers" component={ PageActiveModifiers } />
+			<Route path="/trigered-events" component={ PageTriggeredEvents } />
+			<Route path="/latest-events" component={ PageLatestEvents } />
+		</div>
+	</Router>
 );
 
 export default App;
